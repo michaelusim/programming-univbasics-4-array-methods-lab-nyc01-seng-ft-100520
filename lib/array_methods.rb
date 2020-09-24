3d74f530fb08480e09e12752c8f6fc081b9c6ef7
michael onyeije <monyeije16@saintpeters.edu>
2020-09-24 17:33:16 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,21 +4,25 @@ def using_include(array, element)
 end
 
 def using_sort(array)
+  return array.sort(element)
 
 end
 
 def using_reverse(array)
+  return array.reverse(element)
 
 end
 
 def using_first(array)
-
+  return array.first(element)
 end
 
 def using_last(array)
+  return array.last(element)
 
 end
 
 def using_size(array)
+  return array.size(element)
 
 end
